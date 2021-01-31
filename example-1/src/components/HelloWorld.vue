1ce7a47d27d230c129f919442c63049ba3deb992
nnnyyy <nnnyyy@nate.com>
2021-01-31 23:32:03 +0900

    sass 적용

--- example-1/src/components/HelloWorld.vue
+++ example-1/src/components/HelloWorld.vue
@@ -39,7 +39,7 @@ export default class HelloWorld extends Vue {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
 h3 {
   margin: 40px 0 0;
 }
---
+++ example-1/vue.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+    transpileDependencies: [
+    ],
+    css: {
+        loaderOptions: {
+            scss:  {
+                data: `@import "@/styles/_globalStyles.scss";`
+            }
+        }
+    }
+}
